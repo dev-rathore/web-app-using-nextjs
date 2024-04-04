5b2688ccac730073b19eb3b92e1823b0aedd1eb1
dev-rathore <rathoredvd3001@gmail.com>
2024-04-04 20:48:25 +0530

    first commit

---
+++ src/components/button/button.component.tsx
@@ -0,0 +1,20 @@
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+}) => {
+  return (
+    <button
+      className="px-4 py-2 bg-blue-500 text-white rounded-md"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
