3ca6378fd36170fef8471ffdee37e4fc84ff13b0
dev-rathore <rathoredvd3001@gmail.com>
2024-05-29 15:09:47 +0530

    fix bug

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,9 @@
-import Button from "./button";
 import Error from "./error";
 import Input from "./input";
 import Loader from "./loader";
 import Spinner from "./spinner";
 
 export {
-  Button,
   Error,
   Input,
   Loader,
