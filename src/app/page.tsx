2589f82e951581b5d0ae7265f6457ba7ab6ae10e
dev-rathore <rathoredvd3001@gmail.com>
2024-05-29 14:33:36 +0530

    next/link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/components/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Page() {
   return (
@@ -17,13 +18,13 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <a
+          <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span>
             <ArrowRightIcon className="w-5 md:w-6" />
-          </a>
+          </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           <Image
