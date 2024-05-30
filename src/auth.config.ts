b0638206916a215f79cb7e9b0d25d748d6b7ebf9
dev-rathore <rathoredvd3001@gmail.com>
2024-05-30 19:49:29 +0530

    updated protected route

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -9,10 +9,11 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnLogin = nextUrl.pathname.startsWith('/login');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
+      } else if (isOnLogin && isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
